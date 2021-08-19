38b1e4294dc74cbaeaf8f8953389eaa408d45bd1
Tsungyu Yu <ytyubox@gmail.com>
2021-08-19 11:23:12 +0800

    [test] withOneQuestion_Should_RouteToQuestion

--- QuizeEngine/QuizeEngineTests/FlowTests.swift
+++ QuizeEngine/QuizeEngineTests/FlowTests.swift
@@ -1,28 +1,47 @@
 import XCTest
 class Flow {
     let router: Router
+    let questions: [String]
     
-    init(router: Router) {
+    init(questions: [String], router: Router) {
         self.router = router
+        self.questions = questions
     }
     
-    func start() {}
+    func start() {
+        if questions.isEmpty {return}
+        router.routeTo(question: "any question")
+    }
 }
 protocol Router {
+    func routeTo(question: String)
 }
-    class SpyRouter: Router {
-    let routedQuestionCount = 0
+
+class SpyRouter: Router {
+    var routedQuestionCount = 0
+    func routeTo(question: String) {
+        routedQuestionCount += 1
+    }
 }
 
 
 final class FlowTests: XCTestCase {
     func test_start_withNoQuestion_Should_NotRouteToQuestion() {
         let router = SpyRouter()
-        let sut = Flow(router: router)
+        let sut = Flow(questions: [], router: router)
         
         sut.start()
         
         XCTAssertEqual(router.routedQuestionCount, 0)
+    }
+    
+    func test_start_withOneQuestion_Should_RouteToQuestion() {
+        let router = SpyRouter()
+        let sut = Flow(questions: ["Q1"], router: router)
+        
+        sut.start()
+        
+        XCTAssertEqual(router.routedQuestionCount, 1)
     }
 }
 
